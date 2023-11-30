2576f4ae3217eb8c1d5ff7993d729f0343b06ef4
MTA2002 <104497136+MTA2002@users.noreply.github.com>
2023-11-30 22:48:22 +0300

    Added solution for Average Salary Excluding the Minimum and Maximum Salary

--- python-track/average-salary-excluding-the-minimum-and-maximum-salary.py
+++ python-track/average-salary-excluding-the-minimum-and-maximum-salary.py
@@ -1,8 +1,13 @@
 class Solution:
     def average(self, salary: List[int]) -> float:
-        salary.sort()
         size=len(salary)
+        max_element=salary[0]
+        min_element=salary[0]
         sum=0
-        for i in range(1,size-1):
+        for i in range(size):
             sum+=salary[i]
+            max_element=max(max_element,salary[i])
+            min_element=min(min_element,salary[i])
+        sum-=max_element
+        sum-=min_element
         return sum/(size-2)
