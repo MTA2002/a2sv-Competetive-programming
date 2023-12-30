c80a8bc63094d82be7dcf07e6f79910d7d799db7
MTA2002 <104497136+MTA2002@users.noreply.github.com>
2023-12-30 22:27:19 +0300

    Added solution for Maximum Ice Cream Bars

--- on-boarding/maximum-ice-cream-bars.py
+++ on-boarding/maximum-ice-cream-bars.py
@@ -1,11 +1,24 @@
 class Solution:
     def maxIceCream(self, costs: List[int], coins: int) -> int:
-        costs.sort()
+        max_ = max(costs)
+        sorted_list = [0 for i in range(max_)]
         count = 0
         total = 0
+
+        for cost in costs:
+            sorted_list[cost - 1] += 1
+            
+        costs = []
+        for idx,element in enumerate(sorted_list):
+            costs.extend([idx+1]*element)
+
         for cost in costs:
             total += cost
             if total > coins:
                 break
             count += 1
+            
+
+
         return count
+7
