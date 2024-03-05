059c0c5d075c819cc5ba3a9758866fea78966178
Mahfouz Teyib <104497136+MTA2002@users.noreply.github.com>
2024-03-05 23:25:51 +0300

    Add solution for N-Queens

--- camp_1/leetcode/n-queens.py
+++ camp_1/leetcode/n-queens.py
@@ -2,107 +2,35 @@ class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
         ans = set()
         bucket = [['.'] * n for _ in range(n)]
-        visited = Counter()
+        row_set = set()
+        col_set = set()
+        diagonals = set()
+        anti_diagonals = set()
 
         def backtrack(r,c):
             if r == n:
                 ans.add(tuple(["".join(a) for a in bucket]))
                 return
 
-            if visited[(r,c)] <= 0:
-                visited[(r,c)] += 1
-                
-                for i in range(r,n):
-                    visited[(i,c)] += 1
-                for i in range(r):
-                    visited[(i,c)] += 1
-                for i in range(c,n):
-                    visited[(r,i)] += 1
-                for i in range(c):
-                    visited[(r,i)] += 1
+            if r not in row_set and c not in col_set and (r-c) not in diagonals and (r+c) not in  anti_diagonals:
+                row_set.add(r)
+                col_set.add(c)
+                diagonals.add(r-c)
+                anti_diagonals.add(r+c)
 
-                sr = r
-                sc = c
-
-                while sr < n and sc < n:
-                    visited[(sr,sc)] += 1
-                    sr += 1
-                    sc += 1
-
-                sr = r
-                sc = c
-
-                while sr >= 0 and sc < n:
-                    visited[(sr,sc)] += 1
-                    sr -= 1
-                    sc += 1
-
-                sr = r
-                sc = c
-
-                while sr < n and sc >= 0:
-                    visited[(sr,sc)] += 1
-                    sr += 1
-                    sc -= 1
-
-                sr = r
-                sc = c
-
-                while sr >= 0 and sc >= 0:
-                    visited[(sr,sc)] += 1
-                    sr -= 1
-                    sc -= 1
                 bucket[r][c] = 'Q'
 
                 for i in range(n):
                     backtrack(r+1,i)
 
                 bucket[r][c] = '.'
-                visited[(r,c)] -= 1
-                
-                for i in range(r,n):
-                    visited[(i,c)] -= 1
-                for i in range(r):
-                    visited[(i,c)] -= 1
-                for i in range(c,n):
-                    visited[(r,i)] -= 1
-                for i in range(c):
-                    visited[(r,i)] -= 1
+                row_set.discard(r)
+                col_set.discard(c)
+                diagonals.discard(r-c)
+                anti_diagonals.discard(r+c)
 
-                sr = r
-                sc = c
-
-                while sr < n and sc < n:
-                    visited[(sr,sc)] -= 1
-                    sr += 1
-                    sc += 1
-
-                sr = r
-                sc = c
-
-                while sr >= 0 and sc < n:
-                    visited[(sr,sc)] -= 1
-                    sr -= 1
-                    sc += 1
-
-                sr = r
-                sc = c
-
-                while sr < n and sc >= 0:
-                    visited[(sr,sc)] -= 1
-                    sr += 1
-                    sc -= 1
-
-                sr = r
-                sc = c
-
-                while sr >= 0 and sc >= 0:
-                    visited[(sr,sc)] -= 1
-                    sr -= 1
-                    sc -= 1
                 
         for i in range(n):
             backtrack(0,i)
-        print(len(ans))
 
         return ans
