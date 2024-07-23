5cf141b328eb45a7b24aef733ea306cb6f58dede
Mahfouz Teyib <104497136+MTA2002@users.noreply.github.com>
2024-07-23 17:00:33 +0300

    Created 'D - Maximum Number of Zeroes' by A2SV Hub

---
+++ A2SV G5 - Contest #10 23-Jul-2024/D - Maximum Number of Zeroes 183157.py
@@ -0,0 +1,34 @@
+# Problem: D - Maximum Number of Zeroes - https://codeforces.com/gym/514644/problem/D
+
+from collections import Counter, defaultdict, deque
+from decimal import Decimal
+import sys
+
+def Int():return int(sys.stdin.readline().strip())
+def arr():return list(map(int, sys.stdin.readline().strip().split()))
+def string():return sys.stdin.readline().strip()
+
+
+def solve():
+    # pass
+    size = Int()
+    num1 = arr()
+    num2 = arr()
+    res = Counter()
+    tt =0
+    for i in range(size):
+        if num1[i] != 0:
+            ind = Decimal(-1 * num2[i]) /Decimal( num1[i])
+            res[ind] += 1
+        elif num2[i] == 0:
+            tt += 1
+    mm =res.values()
+    print(max(mm) + tt if mm else tt)
+
+    
+
+
+
+t = 1
+for _ in range(t):
+    solve()
